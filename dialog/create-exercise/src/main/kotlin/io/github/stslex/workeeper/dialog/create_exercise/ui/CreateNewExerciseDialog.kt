1233b470aa5abf7561c7ae6b0aa40a9d9a5877d0
stslex <ilya977.077@gmail.com>
2025-09-03 08:08:28 +0300

    fix ci

--- dialog/create-exercise/src/main/kotlin/io/github/stslex/workeeper/dialog/create_exercise/ui/CreateNewExerciseDialog.kt
+++ dialog/create-exercise/src/main/kotlin/io/github/stslex/workeeper/dialog/create_exercise/ui/CreateNewExerciseDialog.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -25,7 +25,7 @@ fun CreateNewExerciseDialog(
     component: CreateDialogComponent
 ) {
     val dialogHeight = LocalView.current.height.toDp * 0.5f
-    BasicAlertDialog(
+    AlertDialog(
         onDismissRequest = { component.dismiss() },
         modifier = modifier
             .fillMaxWidth()
