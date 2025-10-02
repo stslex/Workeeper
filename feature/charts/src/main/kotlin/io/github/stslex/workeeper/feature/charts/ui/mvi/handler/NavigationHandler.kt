f3f8ad1bbb8a26e5ca9b524c3e836de91565007e
stslex <ilya977.077@gmail.com>
2025-10-02 21:28:08 +0300

    refactor: simplify NavigationHandler and update README for Hilt migration
    This commit refactors the NavigationHandler by removing the unused Handler interface and updates the README to reflect the migration from Koin to Hilt for dependency injection. Key changes include:
    - Simplified NavigationHandler implementation.
    - Expanded README section on Hilt, detailing its usage and structure within the project.

--- feature/charts/src/main/kotlin/io/github/stslex/workeeper/feature/charts/ui/mvi/handler/NavigationHandler.kt
+++ feature/charts/src/main/kotlin/io/github/stslex/workeeper/feature/charts/ui/mvi/handler/NavigationHandler.kt
@@ -1,12 +1,11 @@
 package io.github.stslex.workeeper.feature.charts.ui.mvi.handler
 
-import io.github.stslex.workeeper.core.ui.mvi.handler.Handler
 import io.github.stslex.workeeper.core.ui.navigation.Navigator
 import io.github.stslex.workeeper.feature.charts.ui.mvi.store.ChartsStore.Action
 
 internal class NavigationHandler(
     private val navigator: Navigator,
-) : ChartsComponent, Handler<Action.Navigation> {
+) : ChartsComponent {
 
     override fun invoke(action: Action.Navigation) {
         // todo add navigation
