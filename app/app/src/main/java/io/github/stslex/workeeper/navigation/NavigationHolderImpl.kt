c22ffafc40d1aa27dfa061745baf884096b08764
stslex <ilya977.077@gmail.com>
2026-05-05 16:38:03 +0300

    refactor(navigation): replace WeakReference with direct NavHostController reference
    This commit refactors the NavigationHolderImpl class by replacing the WeakReference for the NavHostController with a direct reference. This change helps avoid memory leaks and ensures proper lifecycle management of the NavHostController in a composable scope. Additionally, the static field is annotated with @Volatile to ensure visibility of changes across threads.

--- app/app/src/main/java/io/github/stslex/workeeper/navigation/NavigationHolderImpl.kt
+++ app/app/src/main/java/io/github/stslex/workeeper/navigation/NavigationHolderImpl.kt
@@ -1,24 +1,33 @@
 package io.github.stslex.workeeper.navigation
 
+import android.annotation.SuppressLint
 import androidx.navigation.NavHostController
 import io.github.stslex.workeeper.core.ui.navigation.NavigatorHolder
-import java.lang.ref.WeakReference
 
 class NavigationHolderImpl : NavigatorHolder, NavigationHolderProducer {
 
     override val navigator: NavHostController
-        get() = requireNotNull(_navigator?.get()) {
+        get() = requireNotNull(_navigator) {
             "NavHostController is not set. Make sure to call rememberNavHostControllerHolder() in a composable scope."
         }
 
     @Synchronized
     override fun produce(navController: NavHostController) {
-        _navigator = WeakReference(navController)
+        _navigator = navController
     }
 
     companion object {
 
+        /**
+         * Need to be static to avoid memory leaks, as NavHostController holds a reference to the Activity context.
+         * Using @Volatile to ensure visibility of changes across threads,
+         * as NavHostController can be accessed from different threads.
+         * Suppressing the lint warning for static field leak,
+         * as we are managing the lifecycle of NavHostController properly,
+         * and it will be set and cleared in a composable scope.
+         **/
+        @SuppressLint("StaticFieldLeak")
         @Volatile
-        private var _navigator: WeakReference<NavHostController>? = null
+        private var _navigator: NavHostController? = null
     }
 }
